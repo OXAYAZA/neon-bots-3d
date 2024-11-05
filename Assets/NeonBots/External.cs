95de4067992ac72fafd76a2c31cedbd72e120253
OXAYAZA <gashishmarihuanovich@gmail.com>
2024-11-05 16:47:31 +0200

    Updated tiles colliders

--- Assets/NeonBots/External.cs
+++ Assets/NeonBots/External.cs
@@ -6,21 +6,21 @@ namespace NeonBots
 {
     public static class External
     {
-#if UNITY_WEBGL
+#if UNITY_WEBGL && !UNITY_EDITOR
         [DllImport("__Internal")]
         public static extern void Fullscreen();
 #else
         public static void Fullscreen() {}
 #endif
 
-#if UNITY_WEBGL
+#if UNITY_WEBGL && !UNITY_EDITOR
         [DllImport("__Internal")]
         public static extern bool IsMobile();
 #else
         public static bool IsMobile() => false;
 #endif
 
-#if UNITY_WEBGL
+#if UNITY_WEBGL && !UNITY_EDITOR
         [DllImport("__Internal")]
         public static extern void ShowPopup(string msg, string type);
 #else
